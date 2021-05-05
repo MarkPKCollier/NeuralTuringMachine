4321b4a1f3aa712d3a896f0ba32902ce4d48752f
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-05-05 15:40:21 +0300

    preparing for run

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,8 +15,8 @@ lspci -nn | grep '\[03'
 
 # run the training
 python3 run_tasks.py --experiment_name experiment --verbose no \
-                      --num_train_steps 1000000 --steps_per_eval 10000 --use_local_impl yes \
-                      --curriculum none --device gpu\
-                      --num_bits_per_vector 3 --num_memory_locations 128\
-                      --max_seq_len 4 --task average_sum \
-                      --num_experts 3
+                      --num_train_steps 1000000 --steps_per_eval 1000 --use_local_impl yes \
+                      --curriculum none --device cpu\
+                      --num_bits_per_vector 3 --num_memory_locations 256\
+                      --max_seq_len 4 --task mta \
+                      --num_experts 2
