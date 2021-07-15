1831a607cc811e72c9b19c0ba3daa0879ff5d4f5
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-07-15 14:12:02 +0300

    saving current status

--- visualize_logs.py
+++ visualize_logs.py
@@ -50,7 +50,7 @@ def create_chart(ticks, granularity=1000, type='error', bits_per_number=4, serie
     plt.locator_params(nbins=5)
     # plt.xticks(new_ticks, **font_settings)
     plt.yticks(**font_settings)
-    plt.title(f'NTM training dynamics for MTA \noperator for three assessments, {chart_title}')
+    plt.title(f'NTM training dynamics for MTA \noperator for two assessments, {chart_title}')
     plt.legend()
     return plt
 
@@ -66,7 +66,7 @@ def save_chart(plot, type, bits_per_number):
 
 
 PARSABLE_PATTERN = re.compile(r'''
-                        (.*:EVAL_PARSABLE:\s)
+                        (.*EVAL_PARSABLE:\s)
                         (?P<step>\d+),
                         (?P<error>\d+\.\d+(e[+-]\d+)?),
                         (?P<loss>\d+\.\d+(e[+-]\d+)?).*''', re.VERBOSE)
@@ -100,6 +100,9 @@ def main(args):
 def main_mta_all(args):
     mem_128_4_bit_steps, mem_128_4_bit_errors, mem_128_4_bit_losses = get_history(
         './trained_models/average_binary_sum_v1/out.log')
+    mem_128_4_bit_steps = [*mem_128_4_bit_steps[9::10], mem_128_4_bit_steps[-1]]
+    mem_128_4_bit_errors = [*mem_128_4_bit_errors[9::10], mem_128_4_bit_errors[-1]]
+    mem_128_4_bit_losses = [*mem_128_4_bit_losses[9::10], mem_128_4_bit_losses[-1]]
     mem_256_6_bit_steps, mem_256_6_bit_errors, mem_256_6_bit_losses = get_history(
         './trained_models/average_binary_sum_v2/6_bits_256_memory_3_experts_contrib/out.log')
     mem_256_8_bit_steps, mem_256_8_bit_errors, mem_256_8_bit_losses = get_history(
@@ -137,6 +140,75 @@ def main_mta_all(args):
     save_chart(plot, type='loss', bits_per_number=args.bits_per_number)
 
 
+def main_mta_2_experts_all(args):
+    mem_128_4_bit_steps, mem_128_4_bit_errors, mem_128_4_bit_losses = get_history(
+        './trained_models/average_binary_sum_v3/4_bits_128_memory_2_experts_contrib/out.log')
+    mem_256_6_bit_steps, mem_256_6_bit_errors, mem_256_6_bit_losses = get_history(
+        './trained_models/average_binary_sum_v3/6_bits_256_memory_2_experts_contrib/out.log')
+    mem_256_8_bit_steps, mem_256_8_bit_errors, mem_256_8_bit_losses = get_history(
+        './trained_models/average_binary_sum_v3/8_bits_256_memory_2_experts_contrib/out.log')
+    mem_256_10_bit_steps, mem_256_10_bit_errors, mem_256_10_bit_losses = get_history(
+        './trained_models/average_binary_sum_v3/10_bits_256_memory_2_experts_contrib/out.log')
+    mem_256_16_bit_steps, mem_256_16_bit_errors, mem_256_16_bit_losses = get_history(
+        './trained_models/average_binary_sum_v3/16_bits_256_memory_2_experts_contrib/out.log')
+    mem_256_16_bit_steps = [*mem_256_16_bit_steps[:12]]
+    mem_256_16_bit_errors = [*mem_256_16_bit_errors[:12]]
+    mem_256_16_bit_losses = [*mem_256_16_bit_losses[:12]]
+
+    series_dict = {
+        '4 bits, 128 memory': mem_128_4_bit_errors,
+        '6 bits, 256 memory': mem_256_6_bit_errors,
+        '8 bits, 256 memory': mem_256_8_bit_errors,
+        '10 bits, 256 memory': mem_256_10_bit_errors,
+        '16 bits, 256 memory': mem_256_16_bit_errors,
+    }
+    max_len = max([len(i) for i in series_dict.values()])
+    for key in series_dict:
+        series_dict[key].extend([np.nan for _ in range(max_len - len(series_dict[key]))])
+    plot = create_chart(series=series_dict, ticks=mem_256_16_bit_steps, granularity=args.granularity, type='error',
+                        bits_per_number=args.bits_per_number)
+    save_chart(plot, type='error', bits_per_number=args.bits_per_number)
+
+    series_dict = {
+        '4 bits, 128 memory': mem_128_4_bit_losses,
+        '6 bits, 256 memory': mem_256_6_bit_losses,
+        '8 bits, 256 memory': mem_256_8_bit_losses,
+        '10 bits, 256 memory': mem_256_10_bit_losses,
+        '16 bits, 256 memory': mem_256_16_bit_losses,
+    }
+    max_len = max([len(i) for i in series_dict.values()])
+    for key in series_dict:
+        series_dict[key].extend([np.nan for _ in range(max_len - len(series_dict[key]))])
+    plot = create_chart(series=series_dict, ticks=mem_256_16_bit_steps, granularity=args.granularity, type='loss',
+                        bits_per_number=args.bits_per_number)
+    save_chart(plot, type='loss', bits_per_number=args.bits_per_number)
+
+
+def main_mta_2_tuple_2_experts_v1_all(args):
+    mem_256_104_bit_steps, mem_256_104_bit_errors, mem_256_104_bit_losses = get_history(
+        './trained_models/mta_v1/104_bits_256_memory_2_experts_local_full_binary_layout/out.log')
+
+    series_dict = {
+        '104 bits, 256 memory': mem_256_104_bit_errors,
+    }
+    max_len = max([len(i) for i in series_dict.values()])
+    for key in series_dict:
+        series_dict[key].extend([np.nan for _ in range(max_len - len(series_dict[key]))])
+    plot = create_chart(series=series_dict, ticks=mem_256_104_bit_steps, granularity=args.granularity, type='error',
+                        bits_per_number=args.bits_per_number)
+    save_chart(plot, type='error', bits_per_number=args.bits_per_number)
+
+    series_dict = {
+        '104 bits, 256 memory': mem_256_104_bit_losses,
+    }
+    max_len = max([len(i) for i in series_dict.values()])
+    for key in series_dict:
+        series_dict[key].extend([np.nan for _ in range(max_len - len(series_dict[key]))])
+    plot = create_chart(series=series_dict, ticks=mem_256_104_bit_steps, granularity=args.granularity, type='loss',
+                        bits_per_number=args.bits_per_number)
+    save_chart(plot, type='loss', bits_per_number=args.bits_per_number)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_path', required=True, type=str,
@@ -147,6 +219,8 @@ if __name__ == '__main__':
                         help='Bits per number')
     args = parser.parse_args()
     if args.log_path == 'mta_all':
-        main_mta_all(args)
+        # main_mta_all(args)
+        # main_mta_2_experts_all(args)
+        main_mta_2_tuple_2_experts_v1_all(args)
     else:
         main(args)
